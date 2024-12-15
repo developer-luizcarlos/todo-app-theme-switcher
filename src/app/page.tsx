09be4b260e6ffd81b9a3a094bbe7cf9319b8955a
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2024-12-15 19:20:08 -0300

    feature: listing tasks and filter its elements turned into a separated component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // hooks and utilities importations
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import Image from "next/image";
 
 // context importation
@@ -9,16 +9,11 @@ import { Context } from "@/components/Context/Context";
 
 // components importation
 import Input from "@/components/Input/Input";
-import TaskWrapper from "@/components/TaskWrapper/TaskWrapper";
+import ListingTasks from "@/components/ListingTasks/ListingTasks";
 import NoData from "@/components/NoData/NoData";
 
-type tasksShowedType = "all" | "active" | "completed";
-
 const Home = () => {
-  const { theme, changeTheme, state, dispatch } = useContext(Context)!;
-
-  // states
-  const [tasksShowed, setTasksShowed] = useState<tasksShowedType>("all");
+  const { theme, changeTheme, state } = useContext(Context)!;
 
   return (
     <div className="w-full h-full">
@@ -44,54 +39,9 @@ const Home = () => {
 
           <article className="rounded mt-5">
             <div>
-              {(state.length) ? state.map((task) => {
-                const taskElement =
-                  <TaskWrapper key={task.id} id={task.id} text={task.content} isCompleted={task.completed} />;
-
-                if(tasksShowed === "all") {
-                  return taskElement;
-                } else if(tasksShowed === "active") {
-                  if(!task.completed) return taskElement;
-                } else if(tasksShowed === "completed") {
-                  if(task.completed) return taskElement;
-                }
-              }) : <NoData />}
+              {(state.length) ? <ListingTasks /> : <NoData />}
             </div>
-            <footer
-              className={
-                theme === "dark"
-                  ? "flex items-center justify-between p-3 bg-very-dark-desaturated-blue text-very-dark-gray text-xl capitalize"
-                  : "flex items-center justify-between p-3 bg-very-light-gray text-dark-grayish-blue text-xl capitalize"}>
-              <small
-                className={theme === "dark" ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}>items left</small>
-              <div className="flex items-center gap-2">
-                <small
-                  className={
-                    theme == "dark"
-                      ? tasksShowed === "all" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
-                      : tasksShowed === "all" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
-                  onClick={() => setTasksShowed("all")}
-                >all</small>
-                <small
-                  className={
-                    theme == "dark"
-                      ? tasksShowed === "active" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
-                      : tasksShowed === "active" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
-                  onClick={() => setTasksShowed("active")}
-                >active</small>
-                <small
-                  className={
-                    theme == "dark"
-                      ? tasksShowed === "completed" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
-                      : tasksShowed === "completed" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
-                  onClick={() => setTasksShowed("completed")}
-                >completed</small>
-              </div>
-              <small
-                className={theme === "dark" ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
-                onClick={() => dispatch({ type: "CLEAR" })}
-              >clear completed</small>
-            </footer>
+
           </article>
         </section>
       </main>
---
+++ src/components/ListingTasks/ListingTasks.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useContext, useState } from "react";
+import { Context } from "../Context/Context";
+import TaskWrapper from "../TaskWrapper/TaskWrapper";
+
+type tasksShowedType = "all" | "active" | "completed";
+
+const ListingTasks = () => {
+  const { state, dispatch, theme } = useContext(Context)!;
+
+  const [tasksShowed, setTasksShowed] = useState<tasksShowedType>("all");
+
+  return (
+    <div>
+      <div>
+        {state.map((task) => {
+          const taskElement =
+            <TaskWrapper key={task.id} id={task.id} text={task.content} isCompleted={task.completed} />;
+
+          if(tasksShowed === "all") {
+            return taskElement;
+          } else if(tasksShowed === "active") {
+            if(!task.completed) return taskElement;
+          } else if(tasksShowed === "completed") {
+            if(task.completed) return taskElement;
+          }
+        })}
+      </div>
+      <footer
+        className={
+          theme === "dark"
+            ? "flex items-center justify-between p-3 bg-very-dark-desaturated-blue text-very-dark-gray text-xl capitalize"
+            : "flex items-center justify-between p-3 bg-very-light-gray text-dark-grayish-blue text-xl capitalize"}>
+        <small
+          className={theme === "dark" ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}>items left</small>
+        <div className="flex items-center gap-2">
+          <small
+            className={
+              theme == "dark"
+                ? tasksShowed === "all" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
+                : tasksShowed === "all" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+            onClick={() => setTasksShowed("all")}
+          >all</small>
+          <small
+            className={
+              theme == "dark"
+                ? tasksShowed === "active" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
+                : tasksShowed === "active" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+            onClick={() => setTasksShowed("active")}
+          >active</small>
+          <small
+            className={
+              theme == "dark"
+                ? tasksShowed === "completed" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
+                : tasksShowed === "completed" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+            onClick={() => setTasksShowed("completed")}
+          >completed</small>
+        </div>
+        <small
+          className={theme === "dark" ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+          onClick={() => dispatch({ type: "CLEAR" })}
+        >clear completed</small>
+      </footer>
+    </div>
+  );
+};
+
+export default ListingTasks;
