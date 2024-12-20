9f24a865cb59bc81ed2053173f39f4f8a2e335d9
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2024-12-20 08:59:29 -0300

    feature: adding a border on Input component when the inner input is focus.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,7 +10,6 @@ import { Context } from "../Context/Context";
 // icons importation
 import { FaCheck } from "react-icons/fa6";
 
-
 const Input = () => {
   // context values
   const { theme, dispatch } = useContext(Context)!;
@@ -21,7 +20,7 @@ const Input = () => {
 
   // functions
   const createTask = (value: string): void => {
-    if(value.trim()) {
+    if (value.trim()) {
       dispatch({ type: "ADD", content: inputValue, completed: false });
     } else {
       alert("It's impossible to create tasks with empty content.");
@@ -32,7 +31,11 @@ const Input = () => {
 
   return (
     <div
-      className={theme == "dark" ? "w-full h-14 bg-very-dark-desaturated-blue rounded flex items-center justify-between" : "w-full h-14 bg-very-light-gray rounded flex items-center justify-between"}
+      className={
+        theme == "dark"
+          ? "w-full h-14 bg-very-dark-desaturated-blue rounded flex items-center justify-between border-2 border-transparent has-[:focus]:border-dark-grayish-blue"
+          : "w-full h-14 bg-very-light-gray rounded flex items-center justify-between border-2 border-transparent has-[:focus]:border-very-dark-desaturated-blue"
+      }
     >
       <span
         className="w-6 h-6 rounded-full border-2 border-dark-grayish-blue ml-2"
@@ -44,10 +47,14 @@ const Input = () => {
         maxLength={40}
         value={inputValue}
         placeholder="Press enter to create a new task"
-        className={theme == "dark" ? "w-full h-full p-2 text-lg text-very-light-grayish-blue bg-transparent outline-none" : "w-full h-full p-2 text-lg text-very-dark-grayish-blue bg-transparent outline-none"}
+        className={
+          theme == "dark"
+            ? "w-full h-full p-2 text-lg text-very-light-grayish-blue bg-transparent outline-none"
+            : "w-full h-full p-2 text-lg text-very-dark-grayish-blue bg-transparent outline-none"
+        }
         onChange={(e) => setInputValue(e.target.value)}
         onKeyDown={(e: KeyboardEvent) => {
-          if(e.key === "Enter") {
+          if (e.key === "Enter") {
             createTask(inputValue);
           }
         }}
