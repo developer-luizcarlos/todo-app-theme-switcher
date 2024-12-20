ac0013abfd880d4e6674de3dbd844b1f76dc67a6
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2024-12-20 08:53:33 -0300

    feature: Improving the visibility of the tasks based on the tasksShowed state, returning an alert component which says that there is no task corresponding to its value.

--- src/components/ListingTasks/ListingTasks.tsx
+++ src/components/ListingTasks/ListingTasks.tsx
@@ -12,56 +12,137 @@ const ListingTasks = () => {
 
   const [tasksShowed, setTasksShowed] = useState<tasksShowedType>("all");
 
+  const allTasks = () => {
+    return state.map((task) => (
+      <TaskWrapper
+        key={task.id}
+        id={task.id}
+        text={task.content}
+        isCompleted={task.completed}
+      />
+    ));
+  };
+
+  const activeTasks = () => {
+    const active = state.filter((task) => !task.completed);
+
+    return active.length ? (
+      active.map((taskActive) => (
+        <TaskWrapper
+          key={taskActive.id}
+          id={taskActive.id}
+          text={taskActive.content}
+          isCompleted={taskActive.completed}
+        />
+      ))
+    ) : (
+      <EmptyList />
+    );
+  };
+
+  const completedTask = () => {
+    const completed = state.filter((task) => task.completed);
+
+    return completed.length ? (
+      completed.map((taskCompleted) => (
+        <TaskWrapper
+          key={taskCompleted.id}
+          id={taskCompleted.id}
+          text={taskCompleted.content}
+          isCompleted={taskCompleted.completed}
+        />
+      ))
+    ) : (
+      <EmptyList />
+    );
+  };
+
+  const showTasks = () => {
+    switch (tasksShowed) {
+      case "all":
+        return allTasks();
+      case "active":
+        return activeTasks();
+      case "completed":
+        return completedTask();
+      default:
+        return <EmptyList />;
+    }
+  };
+
   return (
     <div>
-      <div>
-        {state.map((task) => {
-          const taskElement =
-            <TaskWrapper key={task.id} id={task.id} text={task.content} isCompleted={task.completed} />;
-
-          if(tasksShowed === "all") {
-            return taskElement;
-          } else if(tasksShowed === "active") {
-            return !task.completed ? taskElement : <EmptyList key={task.id} />;
-          } else if(tasksShowed === "completed") {
-            return task.completed ? taskElement : <EmptyList key={task.id} />;
-          }
-        })}
-      </div>
+      <div>{showTasks()}</div>
       <footer
         className={
           theme === "dark"
             ? "flex items-center justify-between p-3 bg-very-dark-desaturated-blue text-very-dark-gray text-xl capitalize"
-            : "flex items-center justify-between p-3 bg-very-light-gray text-dark-grayish-blue text-xl capitalize"}>
+            : "flex items-center justify-between p-3 bg-very-light-gray text-dark-grayish-blue text-xl capitalize"
+        }
+      >
         <small
-          className={theme === "dark" ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}>items left</small>
+          className={
+            theme === "dark"
+              ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer"
+              : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"
+          }
+        >
+          items left
+        </small>
         <div className="flex items-center gap-2">
           <small
             className={
               theme == "dark"
-                ? tasksShowed === "all" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
-                : tasksShowed === "all" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+                ? tasksShowed === "all"
+                  ? "text-blue-700"
+                  : "hover:text-very-light-gray duration-75 ease cursor-pointer"
+                : tasksShowed === "all"
+                ? "text-blue-700"
+                : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"
+            }
             onClick={() => setTasksShowed("all")}
-          >all</small>
+          >
+            all
+          </small>
           <small
             className={
               theme == "dark"
-                ? tasksShowed === "active" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
-                : tasksShowed === "active" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+                ? tasksShowed === "active"
+                  ? "text-blue-700"
+                  : "hover:text-very-light-gray duration-75 ease cursor-pointer"
+                : tasksShowed === "active"
+                ? "text-blue-700"
+                : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"
+            }
             onClick={() => setTasksShowed("active")}
-          >active</small>
+          >
+            active
+          </small>
           <small
             className={
               theme == "dark"
-                ? tasksShowed === "completed" ? "text-blue-700" : "hover:text-very-light-gray duration-75 ease cursor-pointer"
-                : tasksShowed === "completed" ? "text-blue-700" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+                ? tasksShowed === "completed"
+                  ? "text-blue-700"
+                  : "hover:text-very-light-gray duration-75 ease cursor-pointer"
+                : tasksShowed === "completed"
+                ? "text-blue-700"
+                : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"
+            }
             onClick={() => setTasksShowed("completed")}
-          >completed</small>
+          >
+            completed
+          </small>
         </div>
         <small
-          className={theme === "dark" ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer" : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"}
+          className={
+            theme === "dark"
+              ? "hover:text-very-light-grayish-blue duration-75 ease cursor-pointer"
+              : "hover:text-very-dark-grayish-blue duration-75 ease cursor-pointer"
+          }
           onClick={() => dispatch({ type: "CLEAR" })}
-        >clear completed</small>
+        >
+          clear completed
+        </small>
       </footer>
     </div>
   );
